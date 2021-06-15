45d67fa1d501c63383e503c84a6437bc7da23234
Edward Zhan <zhanleewo@gmail.com>
2021-06-15 20:10:34 +0800

    update patch installer

--- install-aifi-patch.sh
+++ install-aifi-patch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 curl https://ai-fi.net/apps/livecd/Counterseal.zip -o Counterseal.zip
-unzip Counterseal.zip counterseal-electron-tmp/
+unzip Counterseal.zip -d counterseal-electron-tmp/
 mv counterseal-electron-tmp/Counterseal/* config/chroot_local-includes/usr/local/ai-fi.net/Counterseal/
 sudo chmod 4755 config/chroot_local-includes/usr/local/ai-fi.net/Counterseal/Counterseal
 rm -rf Counterseal.zip counterseal-electron-tmp
